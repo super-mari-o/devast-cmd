cb56a1ce08952d2001ae29f4c9cc24c0b3c3cf55
マリオ < super-mari-o@users.noreply.github.com>
2025-07-04 21:46:22 +0900

    fix

---
+++ src/lib/ai.ts
@@ -0,0 +1,12 @@
+export const ai = [
+	["", "life", "speed [day / night]", "damages [day / night]", "aggro"],
+	["normal_ghoul", "160", "12 / 22", "8 / 20", "16min"],
+	["fast_ghoul", "120", "18 / 28", "3 / 7", "32min"],
+	["explosive_ghoul", "80", "12 / 23", "6 / 20", "48min"],
+	["radioactive_ghoul", "160", "12 / 23", "5 / 15", "64min"],
+	["armored_ghoul", "800", "12 / 23", "20 / 50", "80min"],
+	["pumpkin_ghoul", "160", "4 / 4", "20 / 30", "16min"],
+	["lapabot_repair", "600", "8 / 8", "30 / 30", "0"],
+	["hal_bot", "800", "12 / 12", "30 / 30", "0"],
+	["tesla_bot", "3000", "10 / 10", "80 / 80", "0"],
+];
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { ai } from "$lib/ai";
+
+    const arr: string[] = [];
+    for (const row of ai.slice(1)) {
+        const [day, night] = row[2]
+            .split("/")
+            .map((v) => v.trim())
+            .map(Number);
+        arr.push(`!AI-speed=${row[0]}:${day}:${night}`);
+    }
+    for (const row of ai.slice(1)) {
+        const [day, night] = row[3]
+            .split("/")
+            .map((v) => v.trim())
+            .map(Number);
+        arr.push(`!AI-damage=${row[0]}:${day}:${night}`);
+    }
+    for (const row of ai.slice(1)) {
+        arr.push(`!AI-life=${row[0]}:${row[1]}`);
+    }
+    arr.push("!harvest-speed=1");
+    arr.push("!xp=1");
+</script>
+
+<textarea>{arr.join("\n")}</textarea>
